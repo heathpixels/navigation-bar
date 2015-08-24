b0b8a668a9f1f89dc63c7dda4e41a3d9f4ad9a42
Karalyn Heath <karalynmh@gmail.com>
2015-08-24 10:48:22 -0700

    Added css for notification bubble.
    Signed-off-by: Karalyn Heath <karalynmh@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
-//Show dialog on hover
-$(document).ready(function(){
+//Show dialog on hover. Position of dialog moves with respect to nav objectives
     $(".objct").hover(function(){
-        $(".arrow-box").css("visibility", "visible");
+        var rect = this.getBoundingClientRect();
+        $(".arrow-box").css("left", rect.left - 150);
+        if ($( window ).width() < 769) {
+          $(".arrow-box").css("left", $(window).width() / 2 - 150);
+        }
+        $(".arrow-box").css("top", rect.bottom + 20);
+        $(".arrow-box").css("display", "inline-block");
     }, function() {});
-//Hide dialog on mouse leave
-	$(".arrow-box").mouseleave(function(){
-	    $(".arrow-box").css("visibility", "hidden");
-	});
-//Change objective onclick
-//change selected class
-//move check mark
-//
+
+    $(".arrow-box").mouseleave(function(){
+      $(".arrow-box").fadeOut();
+    });
+
+    $(".main-header").mouseleave(function(){
+      if(!$('.arrow-box').is(":hover")){
+          $(".arrow-box").fadeOut();
+      }
+      
+
+//Change onclick objective, selected class, check mark, header
 
 	$( ".obj-drop" ).click(function() {
 		var clickedObjct = this;
@@ -23,8 +32,5 @@ $(document).ready(function(){
   		
   		}
 	});
-
-//change header 
-
 
 });
